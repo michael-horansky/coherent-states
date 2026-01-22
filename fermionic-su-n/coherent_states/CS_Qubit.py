sped up calculating the 4index sum by precalculating order 1 mels of fermionic sequences. prepared slog_esp for the qubit slog_overlap, but there is a bug with the sign flip and cut procedure

fermionic-su-n/coherent_states/CS_Qubit.py
@@ -23,6 +23,45 @@ def esp(roots, order):
             partial_esp[j] += roots[i] * partial_esp[j - 1]
     return(partial_esp[order])
 
+def complex_logsumexp(log_a, sgn_a, log_b, sgn_b):
+
+    # regularising
+    if log_a < log_b:
+        log_a, log_b = log_b, log_a
+        sgn_a, sgn_b = sgn_b, sgn_a
+
+    exp_delta = np.exp(log_b - log_a) * (sgn_b / sgn_a)
+
+    return (
+        log_a + np.log(np.abs(1.0 + exp_delta)),
+        sgn_a * np.sign(1.0 + exp_delta)
+    )
+
+def slog_esp(roots, order):
+    # calculates the signed natural log of the elementary symmetric polynomial over [roots] of the given order
+    log_roots = np.log(np.abs(roots))
+    sgn_roots = np.sign(roots)
+
+    log_partial_esp = -np.inf * np.ones(order + 1)
+    sgn_partial_esp = np.ones(order + 1)
+
+    log_partial_esp[0] = 0.0  # log(1)
+    sgn_partial_esp[0] = 1.0
+
+    for i in range(len(roots)):
+        for j in range(min(i + 1, order), 0, -1):
+            # term = x_i * E[j-1]
+            term_log = log_roots[i] + log_partial_esp[j-1]
+            term_sgn = sgn_roots[i] * sgn_partial_esp[j-1]
+
+            # E[j] += term
+            log_partial_esp[j], sgn_partial_esp[j] = complex_logsumexp(
+                log_partial_esp[j], sgn_partial_esp[j],
+                term_log, term_sgn
+            )
+
+    return(sgn_partial_esp[order], log_partial_esp[order])
+
 
 class CS_Qubit(CS_Base):
 
@@ -35,10 +74,24 @@ class CS_Qubit(CS_Base):
     def random_state(cls, M, S, sampling_method):
         if sampling_method == "uniform":
             #random_z = np.random.normal(0.0, 1.0, M) + 1j * np.random.normal(0.0, 1.0, M)
-            random_z = np.concatenate((np.random.normal(1.0, 1.0, S), np.random.normal(1.0, 1.0, M - S)))
+            random_z = np.concatenate((
+                    np.random.normal(1.0, 1.0, S) + 1j * np.random.normal(1.0, 1.0, S),
+                    np.random.normal(1.0, 1.0, M - S) + 1j * np.random.normal(1.0, 1.0, M - S)
+                ))
+            return(cls(M, S, random_z))
+        if sampling_method == "highest_two_orbitals":
+            random_z = np.concatenate((
+                    np.random.normal(100.0, 100.0, S-2) + 1j * np.random.normal(100.0, 100.0, S-2),
+                    np.random.normal(1.0, 1.0, 2) + 1j * np.random.normal(1.0, 1.0, 2),
+                    np.random.normal(0.0, 0.1, M - S) + 1j * np.random.normal(0.0, 0.1, M - S)
+                ))
             return(cls(M, S, random_z))
         if sampling_method == "highest_orbital":
-            random_z = np.concatenate((np.random.normal(100.0, 100.0, S-2), np.random.normal(1.0, 1.0, 2), np.random.normal(0.0, 0.1, M - S)))
+            random_z = np.concatenate((
+                    np.random.normal(100.0, 100.0, S-1) + 1j * np.random.normal(100.0, 100.0, S-1),
+                    np.random.normal(1.0, 1.0, 1) + 1j * np.random.normal(1.0, 1.0, 1),
+                    np.random.normal(0.0, 0.1, M - S) + 1j * np.random.normal(0.0, 0.1, M - S)
+                ))
             return(cls(M, S, random_z))
         return(None)
 
@@ -92,6 +145,51 @@ class CS_Qubit(CS_Base):
         # returns master_matrix_det, master_matrix_inv, master_matrix_alt_inv
 
         return(None, None, None)
+
+
+    def slog_overlap(self, other, c = [], a = []):
+        # We assume that M, S match
+
+        if len(c) != len(a):
+            return(1, -np.inf)
+
+        if len(c) == 0:
+            return(slog_esp(np.conjugate(self.z) * other.z, self.S))
+
+        # c and a contain distinct values, otherwise this is trivially zero
+        if (len(c) > len(set(c)) or len(a) > len(set(a))):
+            return(1, -np.inf)
+
+        # Let's go for the general overlap
+        z_a = []
+        z_b = []
+
+        prefactor = 1.0
+        cur_sign = 1.0
+        cur_pos_a = self.M - 1
+        cur_pos_b = self.M - 1
+        for i in range(len(c) - 1, -1, -1):
+            prefactor *= np.conjugate(self.z[c[i]]) * other.z[a[i]]
+
+            for j in range(cur_pos_a, c[i], -1):
+                # note we omit c[i] itself, as it is set to zero
+                z_a.insert(0, cur_sign * self.z[j])
+            cur_pos_a = c[i] - 1
+            for j in range(cur_pos_b, a[i], -1):
+                # note we omit c[i] itself, as it is set to zero
+                z_b.insert(0, cur_sign * other.z[j])
+            cur_pos_b = a[i] - 1
+            cur_sign *= -1
+        for j in range(cur_pos_a, -1, -1):
+            z_a.insert(0, cur_sign * self.z[j])
+        for j in range(cur_pos_b, -1, -1):
+            z_b.insert(0, cur_sign * other.z[j])
+
+        reduced_overlap_sgn, reduced_overlap_log = slog_esp(np.conjugate(z_a) * z_b, self.S - len(c))
+        prefactor_sgn = np.sign(prefactor)
+        prefactor_log = np.log(np.abs(prefactor))
+
+        return(prefactor_sgn * reduced_overlap_sgn, prefactor_log + reduced_overlap_log)
 
 
 

fermionic-su-n/coherent_states/CS_Thouless.py
@@ -77,15 +77,27 @@ class CS_Thouless(CS_Base):
         if sampling_method == "uniform":
             random_z = np.random.normal(0.0, 1.0, (M-S, S)) + 1j * np.random.normal(0.0, 1.0, (M-S, S))
             return(cls(M, S, random_z))
-        if sampling_method == "highest_orbital":
+        if sampling_method == "highest_two_orbitals":
             centres = np.zeros((M-S, S))
             widths = np.zeros((M-S, S))
             for i in range(M-S):
                 for j in range(S-2):
                     # stable orbitals
-                    widths[i][j] = 0.1
+                    widths[i][j] = 0.001
                 for j in range(S-2, S):
                     # highest two orbitals
+                    widths[i][j] = 0.1
+            random_z = np.random.normal(centres, widths, (M-S, S)) + 1j * np.random.normal(centres, widths, (M-S, S))
+            return(cls(M, S, random_z))
+        if sampling_method == "highest_orbital":
+            centres = np.zeros((M-S, S))
+            widths = np.zeros((M-S, S))
+            for i in range(M-S):
+                for j in range(S-1):
+                    # stable orbitals
+                    widths[i][j] = 0.1
+                for j in range(S-1, S):
+                    # highest orbital (only one, assuming spin separation)
                     widths[i][j] = 10.0
             random_z = np.random.normal(centres, widths, (M-S, S)) + 1j * np.random.normal(centres, widths, (M-S, S))
             return(cls(M, S, random_z))

fermionic-su-n/mol_solver_degenerate.py
@@ -167,12 +167,18 @@ class ground_state_solver():
         #     -lamb: None for no conditioning, or positive float for the conditioning parameter
         #     -sampling_method: magic word for how the CS samples its parameter tensor
         #     -CS: Type of coherent state to use
+        #     -assume_spin_symmetry: if true, |z_alpha> = |z_beta> for the entire sample. False by default
 
-        self.user_log += f"find_ground_state_sampling [N = {kwargs["N"]}, lambda = {kwargs["lamb"]}, sampling method = {kwargs["sampling_method"]}]\n"
+        if "assume_spin_symmetry" in kwargs:
+            assume_spin_symmetry = kwargs["assume_spin_symmetry"]
+        else:
+            assume_spin_symmetry = False
+
+        self.user_log += f"find_ground_state_sampling [N = {kwargs["N"]}, lambda = {kwargs["lamb"]}, sampling method = {kwargs["sampling_method"]}, assume spin symmetry = {assume_spin_symmetry}]\n"
         procedure_diagnostic = []
 
 
-        print(f"Obtaining the ground state with the method \"random sampling\" [CS types = {kwargs["CS"]}, N = {kwargs["N"]}, lambda = {kwargs["lamb"]}, sampling method = {kwargs["sampling_method"]}]")
+        print(f"Obtaining the ground state with the method \"random sampling\" [CS types = {kwargs["CS"]}, N = {kwargs["N"]}, lambda = {kwargs["lamb"]}, sampling method = {kwargs["sampling_method"]}, assume spin symmetry = {assume_spin_symmetry}]")
 
         # We sample around the HF null guess, i.e. Z = 0
         # We include one extra basis vector - the null point itself!
@@ -190,10 +196,16 @@ class ground_state_solver():
 
         CS_sample = [[ground_state_solver.coherent_state_types[kwargs["CS"]].null_state(self.mol.nao, self.S_alpha), ground_state_solver.coherent_state_types[kwargs["CS"]].null_state(self.mol.nao, self.S_beta)]]
         for i in range(kwargs["N"]):
-            CS_sample.append([
-                ground_state_solver.coherent_state_types[kwargs["CS"]].random_state(self.mol.nao, self.S_alpha, kwargs["sampling_method"]),
-                ground_state_solver.coherent_state_types[kwargs["CS"]].random_state(self.mol.nao, self.S_beta, kwargs["sampling_method"])
-                ])
+
+            if assume_spin_symmetry:
+                new_sample_state = ground_state_solver.coherent_state_types[kwargs["CS"]].random_state(self.mol.nao, self.S_alpha, kwargs["sampling_method"])
+                CS_sample.append([new_sample_state, new_sample_state])
+
+            else:
+                CS_sample.append([
+                    ground_state_solver.coherent_state_types[kwargs["CS"]].random_state(self.mol.nao, self.S_alpha, kwargs["sampling_method"]),
+                    ground_state_solver.coherent_state_types[kwargs["CS"]].random_state(self.mol.nao, self.S_beta, kwargs["sampling_method"])
+                    ])
 
         basis_samples_bulk = []
         for i in range(N):
@@ -219,6 +231,7 @@ class ground_state_solver():
                 overlap_matrix[i][j] = CS_sample[i][0].norm_overlap(CS_sample[j][0]) * CS_sample[i][1].norm_overlap(CS_sample[j][1])
         print("-- Overlap matrix:")
         print(overlap_matrix)
+        print(f"[Overlap matrix condition number = {np.linalg.cond(overlap_matrix)}]")
 
         # At what trim number is the condition number maximal?
         S_cond_trim_max = 1
@@ -543,14 +556,22 @@ class ground_state_solver():
 
         #print("lolll", alpha_overlap, beta_overlap)
 
+        # To speed up cross-spin two-electron matrix elements, we prepare a matrix of all first-order sequence overlaps
+        W_alpha = np.zeros((self.mol.nao, self.mol.nao), dtype=complex) # [i][j] = < alpha | f\hc_i f_j | alpha >
+        W_beta = np.zeros((self.mol.nao, self.mol.nao), dtype=complex) # [i][j] = < beta | f\hc_i f_j | beta >
+
         H_one_term = 0.0
         # This is a sum over all mode pairs
         for p in range(self.mol.nao):
             for q in range(self.mol.nao):
+
+                W_alpha[p][q] = pair_a[0].norm_overlap(pair_b[0], [p], [q])
+                W_beta[p][q]  = pair_a[1].norm_overlap(pair_b[1], [p], [q])
+
                 # alpha
-                H_one_term += self.mode_exchange_energy([p], [q]) * pair_a[0].norm_overlap(pair_b[0], [p], [q]) * beta_overlap
+                H_one_term += self.mode_exchange_energy([p], [q]) * W_alpha[p][q] * beta_overlap
                 # beta
-                H_one_term += self.mode_exchange_energy([p], [q]) * pair_a[1].norm_overlap(pair_b[1], [p], [q]) * alpha_overlap
+                H_one_term += self.mode_exchange_energy([p], [q]) * W_beta[p][q] * alpha_overlap
 
         H_two_term = 0.0
 
@@ -668,10 +689,12 @@ class ground_state_solver():
                         prefactor = 0.5 * self.mode_exchange_energy([i, j], [k, l])
 
                         # alpha beta
-                        H_two_term += prefactor * pair_a[0].norm_overlap(pair_b[0], [i], [k]) * pair_a[1].norm_overlap(pair_b[1], [j], [l])
+                        #H_two_term += prefactor * pair_a[0].norm_overlap(pair_b[0], [i], [k]) * pair_a[1].norm_overlap(pair_b[1], [j], [l])
+                        H_two_term += prefactor * W_alpha[i][k] * W_beta[j][l]
 
                         # beta alpha
-                        H_two_term += prefactor * pair_a[0].norm_overlap(pair_b[0], [j], [l]) * pair_a[1].norm_overlap(pair_b[1], [i], [k])
+                        #H_two_term += prefactor * pair_a[0].norm_overlap(pair_b[0], [j], [l]) * pair_a[1].norm_overlap(pair_b[1], [i], [k])
+                        H_two_term += prefactor * W_alpha[j][l] * W_beta[i][k]
 
 
         #print(H_one_term, H_two_term)
Backup. Afterwards, we desymmetrise RHF calculation and fix the printing to work on the supercomputer (plain print on the fly)

mol_el_struct/interpolator_study.py
@@ -19,7 +19,7 @@ import utils.functions
 from molecules_abstract import *
 
 
-cur_molecule = 'Li2'
+cur_molecule = 'H2O'
 
 
 
@@ -27,13 +27,13 @@ cur_molecule = 'Li2'
 #nontrivial_separations = [0.8, 0.9, 0.95, 1.05, 1.1, 1.15, 1.3]
 nontrivial_separations = [0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.05, 1.15, 1.2, 1.3, 1.4, 1.7, 2.0]
 
-N = 50
+N = 100
 N_sub = 50
 rs = "rp"
-freeze_basis = False
+freeze_basis = True
 
 #atasets_to_load = ["RNCS_50_50_rp", "RNCS_50_50_rp_nf", "RNCS_100_50_rp"] # None for default
-datasets_to_load = ["RNCS_50_50_rp"] # None for default
+datasets_to_load = ["B1SCV_i1_100_50_rp"] # None for default
 
 global_ds_label = f"RNCS_{N}_{N_sub}"
 if rs != "ai":
@@ -70,6 +70,9 @@ for ds in datasets_to_load:
     base_sep_datasets_to_load.append(base_sep_dataset_buf[ds])
 
 mol_solvers[1.0].load_data(["self_analysis", "measured_datasets"], base_sep_datasets_to_load)
+
+mol_solvers[1.0].plot_datasets_extra()
+
 mol_solvers[1.0].log.close_journal()
 
 
@@ -92,7 +95,7 @@ err_func_roster = {"$1/\\sqrt{N}$" : None}
 
 cmap = plt.get_cmap("tab10")
 
-for sep_c in [1.0] + nontrivial_separations[:3]:
+for sep_c in [1.0] + nontrivial_separations[:1]:
     for i_ds in range(len(datasets_to_load)):
         ds = datasets_to_load[i_ds]
 
@@ -101,12 +104,48 @@ for sep_c in [1.0] + nontrivial_separations[:3]:
         base_err = mol_solvers[1.0].disk_jockey.metadata[ds]["result_energy_states"]["E_base_err"]
         N_cutoff_space = np.arange(1, 48, 1, dtype = int)
 
-        plt.title(f"${{\\rm Li}}_2$ extrapolation by inv. sqrt law")
-        plt.xlabel("cutoff N")
-        plt.ylabel("E [H]")
-        plt.axhline(y = mol_solvers[sep_c].ci_energy, linestyle = "dashed", label = "$E_g$")
+        #plt.title(f"${{\\rm Li}}_2$ extrapolation by inv. sqrt law")
+        #plt.xlabel()
+        #plt.ylabel()
+        #plt.axhline(y = mol_solvers[sep_c].ci_energy, linestyle = "dashed", label = "$E_g$")
 
-        i_err_func = 1
+        # Taking the points as error-less, we see if the fit error has a minimum
+        E_ext_space = np.zeros(len(N_cutoff_space))
+        E_ext_err_space = np.zeros(len(N_cutoff_space))
+
+        for i in range(len(N_cutoff_space)):
+
+            cur_E_ext, cur_E_ext_err = mol_solvers[sep_c].extrapolate_by_inverse_sqrt(csv_sol, N_cutoff_space[i])
+            E_ext_space[i] = cur_E_ext
+            E_ext_err_space[i] = cur_E_ext_err # * base_err
+
+
+        fig, ax1 = plt.subplots()
+
+        color = 'tab:red'
+        ax1.set_xlabel("cutoff N")
+        ax1.set_ylabel("$E$ [H]", color=color)
+        ax1.plot(N_cutoff_space, E_ext_space, color=color)
+        ax1.tick_params(axis='y', labelcolor=color)
+        ax1.axhline(y = mol_solvers[sep_c].ci_energy, linestyle = "dashed", label = "$E_g$")
+
+        ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+
+        color = 'tab:blue'
+        ax2.set_ylabel("$\\sigma_E$ [H]", color=color)  # we already handled the x-label with ax1
+        ax2.plot(N_cutoff_space, E_ext_err_space, color=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.show()
+
+
+
+        #plt.errorbar(N_cutoff_space, E_ext_space, yerr = E_ext_err_space, capsize = 3, label = r"$E_{\rm ext.}$")
+        #plt.plot(N_cutoff_space, E_ext_space, label = r"$E_{\rm ext.}$")
+        #plt.plot(N_cutoff_space, E_ext_err_space, label = r"$\sigma_{\rm ext.}$")
+
+        r"""i_err_func = 1
         for err_func_label, err_func in err_func_roster.items():
             E_ext_space = np.zeros(len(N_cutoff_space))
             E_ext_err_space = np.zeros(len(N_cutoff_space))
@@ -137,9 +176,9 @@ for sep_c in [1.0] + nontrivial_separations[:3]:
 
         #plt.plot(N_cutoff_space, E_ext_space, label = r"$E_{\rm ext.}$")
         #plt.plot(N_cutoff_space, E_ext_err_space, label = r"$\sigma_{E_{\rm ext.}}$")
-
-        plt.legend()
-        plt.show()
+        """
+        #plt.legend()
+        #plt.show()
 
 
 

mol_el_struct/mol_solver_degenerate.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
@@ -3419,7 +3420,7 @@ class ground_state_solver():
     # Helper methods
 
     def extrapolate_by_inverse_sqrt(self, dataset_dict, min_N = 10, err_func = None):
-        sigma_zero = 1.0 # dummy val of inherent err
+        """sigma_zero = 1.0 # dummy val of inherent err
 
         N_space = []
         E_space = []
@@ -3454,9 +3455,36 @@ class ground_state_solver():
         l_err, c_err = np.sqrt(np.diag(pcov))
 
         E_zero = c_best
-        E_zero_err = c_err / sigma_zero
+        E_zero_err = c_err # / sigma_zero
 
-        return(E_zero, E_zero_err)
+        return(E_zero, E_zero_err)"""
+
+
+        N_space = []
+        E_space = []
+        trim_i = 0
+        for row in dataset_dict:
+            N_space.append(row["N"])
+            E_space.append(row["E [H]"])
+
+        while(N_space[trim_i] < min_N):
+            trim_i += 1
+            if trim_i == len(N_space):
+                trim_i = 0
+                return(0.0, 0.0) # default vals for failed calc
+
+
+        sqinv_N_space = np.array(1/np.sqrt(N_space))
+
+        #self.log.write(str(sqinv_N_space) + "  " + str(E_space))
+
+        res = sp.stats.linregress(sqinv_N_space[trim_i:], E_space[trim_i:])
+
+        #self.log.write(f"For N_min = {min_N}; intercept {res.intercept} +- {res.intercept_stderr}")
+
+        return(res.intercept, res.intercept_stderr)
+
+
 
     def get_dataset_info(self, dataset_label_list, base_err_dict = None):
 
@@ -3494,7 +3522,7 @@ class ground_state_solver():
                 N_space = []
                 for row in self.disk_jockey.data_bulks[dataset_label]["result_energy_states"]:
                     N_space.append(row["N"])
-                N_cutoff_space = N_space[:-2]
+                N_cutoff_space = N_space[:-10] # we don't consider cutoffs for which the number of datapoints is smaller than 10
 
                 self.log.write(f"Cutoff N values range from {N_cutoff_space[0]} to {N_cutoff_space[-1]}.")
 
@@ -3509,7 +3537,7 @@ class ground_state_solver():
                 self.log.write("Extrapolated energy and its uncertainty calculated for each cutoff N value.")
 
                 # As our FINAL GUESS, we select the first datapoint (lowest cutoff) for which all subsequent datapoints are within its interval of uncertainty
-                for i in range(len(N_cutoff_space)):
+                """for i in range(len(N_cutoff_space)):
                     is_consistent_with_subsequent_datapoints = True
                     for j in range(i + 1, len(N_cutoff_space)):
                         if E_ext_space[j] > E_ext_space[i] + E_ext_err_space[i] or E_ext_space[j] < E_ext_space[i] - E_ext_err_space[i]:
@@ -3519,7 +3547,10 @@ class ground_state_solver():
                         final_guess = E_ext_space[i]
                         final_guess_err = E_ext_err_space[i]
                         final_guess_index = i
-                        break
+                        break"""
+                final_guess_index = np.argmin(E_ext_err_space)
+                final_guess = E_ext_space[final_guess_index]
+                final_guess_err = E_ext_err_space[final_guess_index]
                 self.log.write(f"Lowest consistent cutoff N value is {N_cutoff_space[final_guess_index]}; corresponding ext. E = {final_guess:0.5f} +- {final_guess_err:0.5f}")
                 self.log.exit()
             else:
@@ -3580,6 +3611,13 @@ class ground_state_solver():
         self.log.write(f"The atomic orbitals are ordered as follows: {self.mol.ao_labels()}", 3)
         self.log.write(f"The nuclear repulsion energy is {self.mol.energy_nuc()}", 3)
         #print(gto.charge("O"))
+        self.log.write("-" * 20 + " Hilbert space properties " + "-" * 20)
+        assert (self.S + self.mol.spin) % 2 == 0
+        assert (self.S - self.mol.spin) % 2 == 0
+        self.log.print_itemize({
+            "Total number of Slater determinants" : math.comb(self.mol.nao, self.S_alpha) * math.comb(self.mol.nao, self.S_beta),
+            "Total number of S_z-preserving spin-adapted configurations" : int((self.mol.spin + 1) / (self.mol.nao + 1) * math.comb(self.mol.nao + 1, int((self.S - self.mol.spin) / 2)) * math.comb(self.mol.nao + 1, int((self.S + self.mol.spin) / 2) + 1))
+            })
 
         self.log.write("Calculating 1e integrals...", 1)
         AO_H_one = self.mol.intor('int1e_kin', hermi = 1) + self.mol.intor('int1e_nuc', hermi = 1)

mol_el_struct/potential_surface_extrapolator.py
@@ -781,12 +781,14 @@ class potential_surface_extrapolator():
 
             colors = cm.tab10.colors
 
+            E_nuc_surf = self.structure.get_E_nuc_surface()[index_matrix]
+
             for sl in surface_labels:
                 if i_surf is not None:
                     if self.structure.surfaces[sl].meta["i_surf"] not in i_surf:
                         continue
 
-                Z = self.structure.surfaces[sl].E[index_matrix] #np.zeros((self.structure.spans[i_d[1]], self.structure.spans[i_d[0]]))
+                Z = self.structure.surfaces[sl].E[index_matrix] - self.structure.surfaces["FCI[0]"].E[index_matrix] #np.zeros((self.structure.spans[i_d[1]], self.structure.spans[i_d[0]]))
 
                 if "fancy_label" in self.structure.surfaces[sl].meta:
                     plot_label = self.structure.surfaces[sl].meta["fancy_label"]
@@ -798,7 +800,12 @@ class potential_surface_extrapolator():
 
                 col = colors[self.structure.surfaces[sl].meta["i_surf"] % len(colors)]
 
-                ax.plot_surface(
+                """mask = np.isfinite(Z)
+                ax.plot_trisurf(
+                    X_hr[mask].ravel(), Y_hr[mask].ravel(), Z[mask].ravel()
+                )"""
+
+                ax.plot_trisurf(
                     X_hr, Y_hr, Z,
                     color=col,
                     linewidth=0.3,